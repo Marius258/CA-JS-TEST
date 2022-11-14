494b9e080e7fb9b42d38186e4411835b54e33905
Marius258 <marius.kavaliauskas147@gmail.com>
2022-11-14 21:45:12 +0200

    task 9 done

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,25 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title
+    this.director = director
+    this.budget = budget
+  }
+  wasExpensive() {
+    return this.budget > 100000000 ? true : false
+  }
+}
+
+const lordOfTheRings = new Movie('Lord of the rings', 'Peter Jackson', 93000000)
+const piratesOfTheCaribbean = new Movie(
+  'Pirates of the Caribbean',
+  'Rob Marshall',
+  410000000
+)
+
+console.log(lordOfTheRings.wasExpensive())
+console.log(piratesOfTheCaribbean.wasExpensive())
+console.log(piratesOfTheCaribbean)
