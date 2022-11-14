53a33d7f6bcd5bd137ff4f972470899402ace767
Marius258 <marius.kavaliauskas147@gmail.com>
2022-11-14 19:04:41 +0200

    task 4 done

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,4 +8,39 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = 'cars.json'
+const display = document.querySelector('#output')
+
+fetch(ENDPOINT)
+  .then((res) => {
+    if (res.ok) {
+      return res.json()
+    } else {
+      throw new Error('Something went wrong')
+    }
+  })
+  .then((data) => {
+    renderCarList(data, display)
+  })
+  .catch((e) => {
+    console.log(e)
+  })
+
+const renderCarList = (data, parent) => {
+  data.forEach((car) => {
+    const carWrapper = document.createElement('div')
+
+    const brandEl = document.createElement('p')
+    brandEl.textContent = car.brand
+
+    const modelList = document.createElement('ul')
+    car.models.forEach((model) => {
+      const modelEl = document.createElement('li')
+      modelEl.textContent = model
+      modelList.append(modelEl)
+    })
+
+    carWrapper.append(brandEl, modelList)
+    parent.append(carWrapper)
+  })
+}
